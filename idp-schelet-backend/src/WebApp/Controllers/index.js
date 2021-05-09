0ffcbdc9b7f38943adc9ea31bb1b7c4d7c3174f0
Corina MIHAILA <corina.mihaila@stud.acs.upb.ro>
2021-05-09 16:43:51 +0300

    add backend framework

---
+++ idp-schelet-backend/src/WebApp/Controllers/index.js
@@ -0,0 +1,15 @@
+const Router = require('express').Router();
+
+const {
+    authorizeAndExtractTokenAsync
+} = require('../Filters/JWTFilter.js');
+
+/**
+ * TODO import controllers
+ */
+
+/**
+ * TODO add controllers to main router
+ */
+
+module.exports = Router;
