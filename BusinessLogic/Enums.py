8ce4d5982069d75097728b0b3c1343063531a319
Taner Mustafa <tmustafa@fitbit.com>
2021-05-09 22:33:03 +0300

    BusinessLogic Script for measuring iOS app performance metrics

---
+++ BusinessLogic/Enums.py
@@ -0,0 +1,10 @@
+#!/usr/local/bin/python3
+
+from enum import Enum
+
+class Logs(Enum):
+    START_1 = "SpringBoard: (FrontBoard) [com.apple.FrontBoard:Common]"
+    START_2 = "Received request to open"
+
+    LAUNCHED_1 = "SpringBoard: (SpringBoard) [com.apple.SpringBoard:AppSwitcher] SBMainWorkspaceApplicationSceneLayoutElementViewController-sceneID"
+    LAUNCHED_2 = "did end transition to visible YES"
